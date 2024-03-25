7c46a7da440ec25be21e15996006372979b88aa3
Meidesu <antoniomeireles150@gmail.com>
2024-03-24 21:57:21 -0300

    Fundo em madeira, rotação nas aboboras e alternâcia no spawn

--- scenes/fruit.gd
+++ scenes/fruit.gd
@@ -5,18 +5,37 @@ static var id = 0;
 
 @onready var meshes = get_node("Meshes") as Node3D
 @onready var particles = get_node("GPUParticles3D") as GPUParticles3D
+@onready var fruit_mesh = $Meshes/fruitMesh
+@onready var collision = $CollisionShape3D
 
+var rotationDir: Vector3;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	randomize();
 	id += 1
+	
+	# Escolhe aleatoriamente uma direção pra rotacionar
+	rotationDir = Vector3(
+		randf_range(-1, 1),
+		randf_range(-1, 1),
+		randf_range(-1, 1)
+	)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	
 	if global_position.z < -20:
 		queue_free();
-
+	
+	# Aplica a rotação a cada eixo
+	fruit_mesh.rotate_y(rotationDir.z/10);
+	fruit_mesh.rotate_z(rotationDir.x/10);
+	fruit_mesh.rotate_x(rotationDir.y/10);
+	
+	
 
 func _on_input_event(camera: Node, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if cutted: return;
@@ -27,6 +46,7 @@ func cut_fruit():
 	# me apagar e spawnar eu explodindo
 	freeze = true;
 	meshes.visible = false;
+	collision.disabled = true;
 	particles.emitting = true;
 	cutted = true;
 	await particles.finished
--- scenes/world.gd
+++ scenes/world.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
 var fruitScene = preload("res://scenes/fruit.tscn")
-@onready var spawnPoint = get_node("spawnPointFruit")
+@onready var spawnPoint = get_node("spawnPointFruit") as Marker3D;
+@onready var center = $center as Marker3D;
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,11 +15,17 @@ func _process(delta: float) -> void:
 	pass
 
 func spawn_fruit():
+	spawnPoint.global_position.x = randf_range(-3, 3);
+	center.global_position.x = randf_range(-1, 1)
+	
 	var _f = fruitScene.instantiate() as RigidBody3D;
 	_f.global_position = spawnPoint.global_position
 	#_f.global_position.z -= randf_range(-0.5, 0.5)
 	add_child(_f);
-	_f.apply_central_impulse(Vector3(0, 10, 0));
+	
+	var _dir = spawnPoint.global_position.direction_to($center.global_position)
+	_f.apply_central_impulse(_dir * 10);
+	#_f.apply_central_impulse(Vector3(0, 10, 0));
 
 
 func _on_spawn_timer_timeout() -> void:
